8891a82d2e27a19b9da5e69f63f146f6c23c41aa
Madeha Mohammed <madehamohammed36cs@gmail.com>
2026-03-05 19:20:10 +0100

    refactor(Exception): modify InvoiceValidationException

--- src/main/java/at/madeha/intelliinvoice/exception/InvoiceValidationException.java
+++ src/main/java/at/madeha/intelliinvoice/exception/InvoiceValidationException.java
@@ -14,3 +14,19 @@ public class InvoiceValidationException extends RuntimeException {
         super(message, cause);
     }
 }
+/*
+to use the enum error code
+public class InvoiceValidationException extends RuntimeException {
+
+    private final ErrorCode errorCode;
+
+    public InvoiceValidationException(ErrorCode errorCode, String message) {
+        super(message);
+        this.errorCode = errorCode;
+    }
+
+    public ErrorCode getErrorCode() {
+        return errorCode;
+    }
+}
+ */
